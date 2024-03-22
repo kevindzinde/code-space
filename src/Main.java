1739cee001dde6a9f4c39f924653ffa2828e07ff
kevindzinde <kevindzinde2@gmail.com>
2024-03-22 11:09:57 +0200

    init

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+private Main m;
+    public static void main(String[] args) {
+
+        String name ,pyramid;
+         name= "\" Kevin \"\n by kevin";
+         pyramid= """
+                 *
+                * *
+               * * *
+                """;
+        System.out.println(name);
+    }
+}
